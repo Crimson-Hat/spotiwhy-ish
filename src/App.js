691da81c25e1c22b5415b44fa580e74aa5f6517c
AlvinWachiraKE <98809053+Crimson-Hat@users.noreply.github.com>
2023-03-01 04:13:26 +0300

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,87 @@
+import React, { useEffect } from "react";
+import SpotifyWebApi from "spotify-web-api-js";
+import { useStateValue } from "./StateProvider";
+import Player from "./Player";
+import { getTokenFromResponse } from "./spotify";
+import "./App.css";
+import Login from "./Login";
+
+const spotifyApi = new SpotifyWebApi();
+
+function App() {
+  const [{ token }, dispatch] = useStateValue();
+  useEffect(() => {
+    
+    const hash = getTokenFromResponse();
+    window.location.hash = "";
+    let _token = hash.access_token;
+
+    if (_token) {
+      spotifyApi.setAccessToken(_token);
+      dispatch({
+        type: "SET_TOKEN",
+        token: _token,
+      });
+      
+    //const discoverApi = "https://api.spotify.com/v1/views/made-for-x";
+    //const headers =  {
+    //  method: "GET",
+    //  headers: {
+    //    'Authorization': 'Bearer ' + _token
+    //  }
+    //};
+    //
+    //const getDiscoverWeekly = async () => {
+    // await fetch(discoverApi, headers)
+    //  .then((response) => console.log('here >>>', response)) //401: unauthorized, duh
+    //};
+    //
+    //getDiscoverWeekly();
+    
+
+      spotifyApi.getMyTopArtists().then((response) =>
+        dispatch({
+          type: "SET_TOP_ARTISTS",
+          top_artists: response,
+        })
+      );
+
+      dispatch({
+        type: "SET_SPOTIFY",
+        spotify: spotifyApi,
+      });
+
+      spotifyApi.getMe().then((user) => {
+        console.log(user);
+        dispatch({
+          type: "SET_USER",
+          user,
+        });
+      });
+
+      spotifyApi.getUserPlaylists().then((playlists) => {
+        dispatch({
+          type: "SET_PLAYLISTS",
+          playlists,
+        });
+        let playlistTop = playlists.items[0].id;
+        spotifyApi.getPlaylist(playlistTop).then((response) => {
+          console.log("currPlaylist >>>",response);
+          dispatch({
+            type: "SET_DISCOVER_WEEKLY",
+            discover_weekly: response,
+          });
+        });
+      });
+    }
+  }, [token, dispatch]);
+
+  return (
+    <div className="app">
+      {!token && <Login />}
+      {token && <Player spotify={spotifyApi} />}
+    </div>
+  );
+}
+
+export default App;
